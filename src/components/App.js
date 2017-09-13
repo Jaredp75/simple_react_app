3285ec39980185208f099fcbde4bcca725ba0a5d
Jaredp75 <jaredp75@hotmail.com>
2017-09-13 15:07:16 -0400

    Another commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-
 //import logo from './logo.svg';
 import '../styles/App.css';
+import EarthquakeList from './EarthquakeList.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="title"></div>
+        <div className="title">
           <div className="my-header">"Earthquakes!"</div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
       </div>
     );
   }
---
+++ src/components/EarthquakeList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import earthquakes from '../data/earthquakes.js';
+
+
+
+
+export default class EarthquakeList extends Component {
+  render() {
+  let earthquakeList = earthquakes.map((earthquake) => {
+    return (
+      <div className="col-sm-6" key={FILL_ME_IN_WITH_A_UNIQUE_KEY}>
+  <div className="card" >
+    <div className="card-block">
+      <h4 className="card-title">{FILL_ME_IN_WITH_THE_PLACE}</h4>
+      <h6 className="card-subtitle mb-2 text-muted">Magnitude: {FILL_ME_IN_WITH_THE_MAGNITUDE_MAG}</h6>
+      <h6 className="card-subtitle mb-2 text-muted">Time: {moment(FILL_ME_IN_WITH_THE_TIME).format('llll')}</h6>
+      <p className="card-text">Coordinates: {FILL_ME_IN_WITH_THE_COORDINATES}</p>
+
+      <a href={FILL_ME_IN_WITH_THE_URL} className="card-link">USGS Event Link</a>
+
+    </div>
+  </div>
+</div>
+    )
+  });
+
+
+    return (
+      <section>
+        <div className="quake-list">
+          <div className="row">
+          {earthquakeList}
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
